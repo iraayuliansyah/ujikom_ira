a7a5ec0140dcb57ac878425b2b8a73970e261be1
unknown <irayuliansyah@gmail.com>
2025-04-05 19:06:26 +0700

    menambahkan komentar di file css

--- register.php
+++ register.php
@@ -90,8 +90,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
             <br><br>
 
-
-
             <button type="submit">Daftar</button> <!-- Tombol submit -->
         </form>
 
